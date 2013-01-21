bde1140905ed3699c48930448db21863d179573f
RoR Learner <murali8134@gmail.com>
2013-01-22 01:30:40 +0530

    Initial commit at new location

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '83f2f9794f3fe2b2afe431fa69e256c30f30b9eaf2e7bc46c64c4f50ae776878f5bb0c876fc8eff97dfd7885aac32badcd35d7ef7a606890e76b022a00c94859'
